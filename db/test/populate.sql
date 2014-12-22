5d384338f97d890acec4af8f2db5fdf1db7322e1
Melanie L. Fryman <melanie@frymanet.com>
2014-12-22 12:03:40 -0600

    comment out populate notes

--- db/test/populate.sql
+++ db/test/populate.sql
@@ -6,10 +6,10 @@ DELETE FROM users;
 
 INSERT INTO users (id,username,password,avatar,token) VALUES (1,'bob','$2a$08$mrZQOHperHfwQrc1au5CIecSwA6sy1VceDQIEu7SrzKA/qcXcMsoG', 'a.png', 'tok');
 
-INSERT INTO notes (title,body,tags) VALUES ('a1', 'b', 'c,d,e');
-INSERT INTO notes (title,body,tags) VALUES ('a2', 'b', 'c,d,e');
-INSERT INTO notes (title,body,tags) VALUES ('a3', 'b', 'c,d,e');
-
-DELETE FROM notes;
-
-INSERT INTO notes (id,title,body,tags) VALUES (1,'a','b', 'c,d,e');
+-- INSERT INTO notes (title,body,tags) VALUES ('a1', 'b', 'c,d,e');
+-- INSERT INTO notes (title,body,tags) VALUES ('a2', 'b', 'c,d,e');
+-- INSERT INTO notes (title,body,tags) VALUES ('a3', 'b', 'c,d,e');
+--
+-- DELETE FROM notes;
+--
+-- INSERT INTO notes (id,title,body,tags) VALUES (1,'a','b', 'c,d,e');
